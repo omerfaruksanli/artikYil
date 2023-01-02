70d28585d010925578c1e85612b17284de4db8bd
omerfaruksanli <119735918+omerfaruksanli@users.noreply.github.com>
2023-01-02 12:56:23 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+      int yil ;
+      Scanner input = new Scanner(System.in);
+
+        System.out.print(" Lütfen Yıl Giriniz : ");
+        yil = input.nextInt();
+
+        if(yil % 100 == 0) {
+            if(yil % 400 == 0){
+                System.out.print(yil + " bir artık yıldır !");
+            } else{
+                System.out.print(yil + " bir artık yıl değildir ! ");
+            }
+        } else if (yil%4==0) {
+            System.out.print(yil + " bir artık yıldır ! ");
+        } else {
+            System.out.print(yil + " bir artık yıl değildir !");
+        }
+
+
+    }
+}
